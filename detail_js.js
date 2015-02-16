7d4c33db020811012fa57a2f32c1832e6606864e
Meenakshi Agrawal <m@meenakshi.im>
2015-02-16 14:36:27 +0530

    changes done

--- detail_js.js
+++ detail_js.js
@@ -1,8 +1,13 @@
 function details(){
 
   var parts = window.location.href.split('?')[1].split('=');
-  getJSON("http://api.themoviedb.org/3/movie/"+parts[1]+"?api_key=11f47f644c4ac312a64a760d25516450").then(function(data) {
-    document.getElementById("movie-poster").src= "http://image.tmdb.org/t/p/w500"+data.poster_path;
+  image_interval(parts[1]);
+  getJSON("http://api.themoviedb.org/3/movie/"+parts[1]+"?api_key=11f47f644c4ac312a64a760d25516450&append_to_response=trailers").then(function(data) {
+    
+
+    document.getElementById("trailer").addEventListener("click", function(){
+    window.location.href="https://www.youtube.com/watch?v="+data.trailers.youtube[0]["source"];
+});
 
     var movie_duration=document.getElementById("movie-duration");
     var title_runtime=document.createTextNode(data.title +" ( "+data.runtime+" minutes )");
@@ -24,20 +29,76 @@ function details(){
     var synopsis_data =document.createTextNode(data.overview);
     synopsis.appendChild(synopsis_data);
 
-    getJSON("http://api.themoviedb.org/3/movie/"+parts[1]+"/credits?api_key=11f47f644c4ac312a64a760d25516450").then(function(new_data){
+    getJSON("http://api.themoviedb.org/3/movie/"+parts[1]+"/credits?api_key=11f47f644c4ac312a64a760d25516450").then(function(data){
     var cast = document.getElementById("cast");
     var i = 0;
-    var text=" "+new_data.cast[0]["name"];
+    var text=" "+data.cast[0]["name"];
     for(i=0;i<6;i++)
     {
-        text=text + ", "+new_data.cast[i]["name"];
+        text=text + ", "+data.cast[i]["name"];
     }
     var cast_data=document.createTextNode(text);
     cast.appendChild(cast_data);
 
+    getJSON("http://api.themoviedb.org/3/movie/"+parts[1]+"/reviews?api_key=11f47f644c4ac312a64a760d25516450").then(function(data){
+ 
+    var review = document.getElementById("review");
 
-    var director = document.getElementById("director");
-    var reviews = document.getElementById("movie-review");
+    var i;
+    var author_div;
+    var content_div;
+    var author_h5;
+    var content_h5;
+    var author_h5_text;
+    var content_h5_text;
+     var author_data;
+     var content_data;
+     var  content_data_p;
+     var author_data_p;
+    
+    author_div= document.createElement("div");
+    content_div= document.createElement("div");
+
+    author_h5= document.createElement("h5");
+    content_h5= document.createElement("h5");
+
+
+    author_h5_text=document.createTextNode("Author: ");
+    content_h5_text=document.createTextNode("Content: ");
+
+    author_data_p=document.createElement("p");
+    author_data_p.setAttribute('class','author_content_p');
+    author_data=document.createTextNode(data.results[0]["author"]);
+
+   content_data_p=document.createElement("p");
+    content_data_p.setAttribute('class','author_content_p');
+    content_data=document.createTextNode(data.results[0]["content"]);
+
+    review.appendChild(author_div);
+    review.appendChild(content_div);
+
+    author_div.appendChild(author_h5);
+    author_div.appendChild(author_data_p);
+
+    content_div.appendChild(content_h5);
+    content_div.appendChild(content_data_p);
+
+    author_h5.appendChild(author_h5_text);
+    content_h5.appendChild(content_h5_text);
+
+    author_data_p.appendChild(author_data);
+    content_data_p.appendChild(content_data);
+
+    content_data_p.setAttribute('class','module line-clamp');
+
+    var button = document.createElement('button');
+    var button_text=document.
+    content_div.appendChild(button);
+
+    },function(status) {
+        alert("hi"+status);
+    });
+
     },function(status) {
         alert("hi"+status);
     });
@@ -65,3 +126,21 @@ var getJSON = function(url) {
     });
 };  
 
+var x=0;
+var images =[];
+function displayNextImage(){
+               x = (x === images.length - 1) ? 0 : x + 1;
+              document.getElementById("movie-poster").src = "http://image.tmdb.org/t/p/w500"+images[x];
+          }
+function image_interval(id) {
+    getJSON("http://api.themoviedb.org/3/movie/"+id+"/images?api_key=11f47f644c4ac312a64a760d25516450").then(function(data){
+        for (i=0;i<data.backdrops.length;i++)
+        {
+        images[i]=data.backdrops[i]["file_path"];
+        }
+        document.getElementById("movie-poster").src = "http://image.tmdb.org/t/p/w500"+images[x];
+        setInterval(displayNextImage, 4000);
+         }, function(status) {
+        alert(status);
+    });
+}
